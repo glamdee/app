4e7694b639c5659bcaee5ce61e502dfe50dbde96
glamdee <108859003+glamdee@users.noreply.github.com>
2022-07-07 18:03:32 +1000

    Update uniandpancake.js

--- uniandpancake.js
+++ uniandpancake.js
@@ -1,3 +1,4 @@
 db.getCollection("tokensdev").find({"$or":[{"chainId":1},{"$or":[{"$and":[{"symbol":"UNI-V2"},{"name":"Uniswap V2"}]},{"$and":[{"symbol":"Cake-LP"},{"name":"Pancake LPs"}]}]}]})
 #new comment in development branch#
 #extra comment from fix#
+#new comment after merge#
