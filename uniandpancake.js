a246a54d14d5b836d9367c2ea62aed8b41392545
glamdee <108859003+glamdee@users.noreply.github.com>
2022-07-07 16:52:13 +1000

    Update uniandpancake.js

--- uniandpancake.js
+++ uniandpancake.js
@@ -1,1 +1,1 @@
-db.getCollection("tokens").find({"$and":[{"chainId":1},{"$or":[{"$and":[{"symbol":"UNI-V2"},{"name":"Uniswap V2"}]},{"$and":[{"symbol":"Cake-LP"},{"name":"Pancake LPs"}]}]}]})
+db.getCollection("tokensdev").find({"$and":[{"chainId":1},{"$or":[{"$and":[{"symbol":"UNI-V2"},{"name":"Uniswap V2"}]},{"$and":[{"symbol":"Cake-LP"},{"name":"Pancake LPs"}]}]}]})
