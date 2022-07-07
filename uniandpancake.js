4cf2e01e0c5b9170f45f8e60ed34edd2520654fe
Gavin Mach <gavinmach@gmail.com>
2022-07-07 17:52:48 +1000

    move single line

--- uniandpancake.js
+++ uniandpancake.js
@@ -1,4 +1,3 @@
-db.getCollection("tokensdev").find({"$or":[{"chainId":1},{"$or":[{"$and":[{"symbol":"UNI-V2"},{"name":"Uniswap 
-V2"}]},{"$and":[{"symbol":"Cake-LP"},{"name":"Pancake LPs"}]}]}]})
+db.getCollection("tokensdev").find({"$or":[{"chainId":1},{"$or":[{"$and":[{"symbol":"UNI-V2"},{"name":"Uniswap V2"}]},{"$and":[{"symbol":"Cake-LP"},{"name":"Pancake LPs"}]}]}]})
 #new comment in development branch#
 #extra comment from fix#
